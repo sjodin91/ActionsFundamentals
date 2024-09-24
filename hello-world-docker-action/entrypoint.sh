32a551752264bbf708ff34fae612346ffa2406e8
Edward <sjodin_91@hotmail.com>
2024-09-24 15:17:08 +0200

    eftermiddag3

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -2,4 +2,4 @@
 
 echo "hello $1"
 
-echo "time=$(date) fattar almost" >> $GITHUB_OUTPUT
+echo "time=$(date) entrypointen som kör echo hello och tiden och skickar tillbaka som github output" >> $GITHUB_OUTPUT
